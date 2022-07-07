40fae5fdd690d7a3ea0ed9833ab1362a0fc3b521
ElCollo <isanbaev_ildar@mail.ru>
2022-07-07 17:45:29 +0700

    Create 54.js

---
+++ 54.js
@@ -0,0 +1,14 @@
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+   if (arr.length === 0) {
+      return 0;
+   }
+   let incr = 0;
+   let validSmileys = [":)", ";)", ":D", ";D", ":-D", ":~D", ":-)", ":~)", ";~D", ";~)", ";-D", ";-)"]
+   for (let face of arr) {
+      if (validSmileys.includes(face)) {
+         incr++;
+      }
+   }
+   return incr;
+}
