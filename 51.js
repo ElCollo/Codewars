f00e2b51648f10c9cdffb8c42e9896c46d286ce6
ElCollo <isanbaev_ildar@mail.ru>
2022-07-07 17:42:02 +0700

    Create 51.js

---
+++ 51.js
@@ -0,0 +1,15 @@
+function isValidWalk(walk) {
+   //insert brilliant code here
+   let ns = 0, ew = 0;
+   if (walk.length === 10) {
+      for (let i of walk) {
+         if (i == 'n') ns += 1;
+         if (i == 's') ns -= 1;
+         if (i == 'e') ew += 1;
+         if (i == 'w') ew -= 1;
+      }
+   }
+   else
+      return false
+   return ns === 0 && ew === 0;
+}
