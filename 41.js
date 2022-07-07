f374d9686a54fc8e1c7a77895032cfb336ae6e47
ElCollo <isanbaev_ildar@mail.ru>
2022-07-07 17:23:13 +0700

    Create 41.js

---
+++ 41.js
@@ -0,0 +1,9 @@
+function solution(string) {
+   let result = '';
+
+   for (let i = 0; i < string.length; i += 1) {
+      string[i].charCodeAt() > 96 ? result += string[i] : result += ` ${string[i]}`
+   }
+
+   return result;
+}
