1aea2e75a26f8d05d452c7cb58445e4b3816e6a2
ElCollo <isanbaev_ildar@mail.ru>
2022-07-07 17:21:13 +0700

    Create 39.js

---
+++ 39.js
@@ -0,0 +1,8 @@
+function DNAStrand(dna) {
+   var letters = { 'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C' };
+   var arr = [];
+   for (var i = 0; i < dna.length; i++) {
+      arr[i] = letters[dna[i]];
+   }
+   return arr.join('');
+}
